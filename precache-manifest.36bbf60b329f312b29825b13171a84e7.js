881a8b84ff3d94435a650b370d9883c7c07cdb24
SergiyMi <bladeneo@i.ua>
2019-12-16 12:59:51 +0200

    Updates

---
+++ precache-manifest.36bbf60b329f312b29825b13171a84e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d00ea204a13f0b109e5d7ccfcc0d123",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "360756aeab2d7baefa6c",
+    "url": "/goit-react-hw-04-movies/static/css/main.a2df62ac.chunk.css"
+  },
+  {
+    "revision": "d640289fadd63a12137a",
+    "url": "/goit-react-hw-04-movies/static/js/2.d0b6ea38.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/goit-react-hw-04-movies/static/js/2.d0b6ea38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "360756aeab2d7baefa6c",
+    "url": "/goit-react-hw-04-movies/static/js/main.4b93f2dd.chunk.js"
+  },
+  {
+    "revision": "fb96615667c90fd15c7a",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.96c171c8.js"
+  }
+]);
